f75e4f9a51bd61dc5a76d10f4eaa443308d2fd37
sakshichoudhary12 <137709066+sakshichoudhary12@users.noreply.github.com>
2024-09-17 15:45:20 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "../pages/HomePage/HomePage";
+import AddNote from "../pages/AddNotePage/AddNotePage";
+import EditNote from "../pages/EditNotePage/EditNotePage";
+import Notes from "../pages/NotesPage/NotesPage";
+import SingleNote from "./SingleNotePage/SingleNotePage";
+
+
+export { Home, AddNote, Notes, EditNote, SingleNote };
