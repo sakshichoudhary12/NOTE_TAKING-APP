8bc2d3b56afcf3acdf71f531d5c64c67e221f0a8
sakshichoudhary12 <137709066+sakshichoudhary12@users.noreply.github.com>
2024-09-17 15:43:08 +0530

    Add files via upload

---
+++ src/pages/EditNotePage/EditNotePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EditNoteForm from "../../features/notes/EditNoteForm"
+
+const EditNotePage = () => {
+  return (
+     <EditNoteForm />
+  )
+}
+
+
+export default EditNotePage;
