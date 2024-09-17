7b8e6b80bcd7260472b409ffdec7c2bd3e392d61
sakshichoudhary12 <137709066+sakshichoudhary12@users.noreply.github.com>
2024-09-17 15:41:59 +0530

    Add files via upload

---
+++ src/pages/AddNotePage/AddNotePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddNoteForm from "../../features/notes/AddNoteForm";
+
+
+const AddNotePage = () => {
+  return (
+    <div>
+      <AddNoteForm  />
+    </div>
+  )
+}
+
+export default AddNotePage;
